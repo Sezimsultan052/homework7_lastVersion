7ef5f7e32b2cb1cf768196b0f85f46e55a3b67f5
Sezim <sezimsultan052@gmail.com>
2021-09-13 17:23:31 +0600

    7-ая домашка, окончательная версия

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Hero heroes[] = new Hero[3];
         heroes[0] = new Medic(100, 10,AbilityType.SAVE_DAMAGE_AND_REVERT);
-        heroes[1] = new Magic(200, 20, AbilityType.CRITICAL_DAMAGE);
-        heroes[2] = new Medic(300, 30, AbilityType.HEAL);
+        heroes[1] = new Warrior(200, 20, AbilityType.CRITICAL_DAMAGE);
+        heroes[2] = new Magic(300, 30, AbilityType.HEAL);
 
         for(Hero hero: heroes){
             hero.applySuperAbility(hero.getAbilityType());
